d882a7274764b9848a90b9086c2906c4b6df9349
Luis Martinez <martinez.s.luis@gmail.com>
2018-10-07 13:53:38 +0200

    Updates

---
+++ precache-manifest.b715a61b40ae3b8c4db7c50cfde1b342.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/social-card/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "90898db2d41db3b4b6f9",
+    "url": "/social-card/static/css/main.d0e3ac06.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/social-card/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "f561b040ace993b95ae1",
+    "url": "/social-card/static/js/1.f561b040.chunk.js"
+  },
+  {
+    "revision": "c7096105dc4f6d5a29d1",
+    "url": "/social-card/static/js/runtime~main.c7096105.js"
+  },
+  {
+    "revision": "90898db2d41db3b4b6f9",
+    "url": "/social-card/static/js/main.90898db2.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/social-card/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/social-card/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/social-card/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f561b040ace993b95ae1",
+    "url": "/social-card/static/css/1.1c91708c.chunk.css"
+  },
+  {
+    "revision": "56b85322c4783ebbae81117a2c2e42b6",
+    "url": "/social-card/index.html"
+  }
+];
